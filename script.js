4b3016c42243a835891316bce710a5d912f8ead8
Raminn77 <sfrlramin@gmail.com>
2025-10-16 01:31:49 +0400

    hesablama

---
+++ script.js
@@ -0,0 +1,7 @@
+let a = prompt(`reqem yazin`)
+let b = prompt(`ikinci reqem yazin`)
+
+console.log(`cemi ${Number(a) + Number(b) }`)
+console.log(`ferqi ${Number(a) - Number(b) }`)
+console.log(`hasili ${Number(a) * Number(b) }`)
+console.log(`bolmesi ${Number(a) / Number(b) }`)
