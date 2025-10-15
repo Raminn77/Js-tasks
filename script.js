bb37402b6aec032b83589d083229322c556d0664
Raminn77 <sfrlramin@gmail.com>
2025-10-16 01:50:13 +0400

    hesablama

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let a = prompt(`reqem yazin`)
 let b = prompt(`ikinci reqem yazin`)
 
-console.log(`cemi ${Number(a) + Number(b) }`)
-console.log(`ferqi ${Number(a) - Number(b) }`)
-console.log(`hasili ${Number(a) * Number(b) }`)
-console.log(`bolmesi ${Number(a) / Number(b) }`)
+if (Number(a) > Number(b)) {
+    console.log(`a b'den boyukdur`)
+} else if (Number(a) < Number(b)) {
+    console.log(`a b'den kicikdi`)
+} else {
+    console.log(`a ve b beraberdir`)
+}
