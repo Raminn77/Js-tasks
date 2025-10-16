1d335d1c842be03164a76b1b287be5f632df5802
Raminn77 <sfrlramin@gmail.com>
2025-10-16 23:26:06 +0400

    Yaşa Görə Mesaj Göstərmək 1

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-let username = "Sara";
-let age = 34;
-let isStudent = false;
-let weight = 58.3;
-let country = "Azərbaycan";
-let isMarried;
-let favoriteColor = null;
-let phoneNumber = "0123456789";
+let a = prompt(`Adinizi yazin`)
+let b = prompt(`yasinizi yazin`)
 
-console.log(typeof username)
-console.log(typeof age)
-console.log(typeof isStudent)
-console.log(typeof weight)
-console.log(typeof country)
-console.log(typeof isMarried)
-console.log(typeof favoriteColor)
-console.log(typeof phoneNumber)
+if (Number(b) > 18) {
+    console.log(`Salam, ${a} siz artiq boyuksunuz `)
+}else{
+    console.log(`Salam, ${a} siz hele kiciksiniz `)
+}
